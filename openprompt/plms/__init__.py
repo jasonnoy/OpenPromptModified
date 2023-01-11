6504ea0528b84f8ebf53c458bef25c317029f077
Junhui Ji <juji7690@uni.sydney.edu.au>
2023-01-11 16:16:59 +0800

    Add distilBERT support

--- openprompt/plms/__init__.py
+++ openprompt/plms/__init__.py
@@ -7,32 +7,38 @@ from .mlm import MLMTokenizerWrapper
 from .seq2seq import T5LMTokenizerWrapper, T5TokenizerWrapper
 from .lm import LMTokenizerWrapper
 from transformers import BertConfig, BertTokenizer, BertModel, BertForMaskedLM, \
-                         RobertaConfig, RobertaTokenizer, RobertaModel, RobertaForMaskedLM, \
-                         AlbertTokenizer, AlbertConfig, AlbertModel, AlbertForMaskedLM, \
-                         T5Config, T5Tokenizer, T5ForConditionalGeneration, \
-                         OpenAIGPTTokenizer, OpenAIGPTLMHeadModel, OpenAIGPTConfig, \
-                         GPT2Config, GPT2Tokenizer, GPT2LMHeadModel, \
-                         OPTConfig, OPTForCausalLM, \
-                         ElectraConfig, ElectraForMaskedLM, ElectraTokenizer
+    RobertaConfig, RobertaTokenizer, RobertaModel, RobertaForMaskedLM, \
+    AlbertTokenizer, AlbertConfig, AlbertModel, AlbertForMaskedLM, \
+    T5Config, T5Tokenizer, T5ForConditionalGeneration, \
+    OpenAIGPTTokenizer, OpenAIGPTLMHeadModel, OpenAIGPTConfig, \
+    GPT2Config, GPT2Tokenizer, GPT2LMHeadModel, \
+    OPTConfig, OPTForCausalLM, \
+    ElectraConfig, ElectraForMaskedLM, ElectraTokenizer, \
+    DistilBertConfig, DistilBertTokenizer, DistilBertModel
 from collections import namedtuple
 from yacs.config import CfgNode
 
 from openprompt.utils.logging import logger
 
-
-ModelClass = namedtuple("ModelClass", ('config', 'tokenizer', 'model','wrapper'))
+ModelClass = namedtuple("ModelClass", ('config', 'tokenizer', 'model', 'wrapper'))
 
 _MODEL_CLASSES = {
+    'distilbert': ModelClass(**{
+        'config': DistilBertConfig,
+        'tokenizer': DistilBertTokenizer,
+        'model': DistilBertModel,
+        'wrapper': MLMTokenizerWrapper,
+    }),
     'bert': ModelClass(**{
         'config': BertConfig,
         'tokenizer': BertTokenizer,
-        'model':BertForMaskedLM,
+        'model': BertForMaskedLM,
         'wrapper': MLMTokenizerWrapper,
     }),
     'roberta': ModelClass(**{
         'config': RobertaConfig,
         'tokenizer': RobertaTokenizer,
-        'model':RobertaForMaskedLM,
+        'model': RobertaForMaskedLM,
         'wrapper': MLMTokenizerWrapper
     }),
     'albert': ModelClass(**{
@@ -53,13 +59,13 @@ _MODEL_CLASSES = {
         'model': GPT2LMHeadModel,
         'wrapper': LMTokenizerWrapper
     }),
-    't5':ModelClass(**{
+    't5': ModelClass(**{
         'config': T5Config,
         'tokenizer': T5Tokenizer,
         'model': T5ForConditionalGeneration,
         'wrapper': T5TokenizerWrapper
     }),
-    't5-lm':ModelClass(**{
+    't5-lm': ModelClass(**{
         'config': T5Config,
         'tokenizer': T5Tokenizer,
         'model': T5ForConditionalGeneration,
@@ -84,7 +90,7 @@ def get_model_class(plm_type: str):
     return _MODEL_CLASSES[plm_type]
 
 
-def load_plm(model_name, model_path, specials_to_add = None):
+def load_plm(model_name, model_path, specials_to_add=None):
     r"""A plm loader using a global config.
     It will load the model, tokenizer, and config simulatenously.
 
@@ -97,12 +103,12 @@ def load_plm(model_name, model_path, specials_to_add = None):
         :obj:`model_config`: The config of the pretrained model.
         :obj:`wrapper`: The wrapper class of this plm.
     """
-    model_class = get_model_class(plm_type = model_name)
+    model_class = get_model_class(plm_type=model_name)
     model_config = model_class.config.from_pretrained(model_path)
     # you can change huggingface model_config here
     # if 't5'  in model_name: # remove dropout according to PPT~\ref{}
     #     model_config.dropout_rate = 0.0
-    if 'gpt' in model_name: # add pad token for gpt
+    if 'gpt' in model_name:  # add pad token for gpt
         specials_to_add = ["<pad>"]
         # model_config.attn_pdrop = 0.0
         # model_config.resid_pdrop = 0.0
@@ -111,14 +117,11 @@ def load_plm(model_name, model_path, specials_to_add = None):
     tokenizer = model_class.tokenizer.from_pretrained(model_path)
     wrapper = model_class.wrapper
 
-
     model, tokenizer = add_special_tokens(model, tokenizer, specials_to_add=specials_to_add)
 
     if 'opt' in model_name:
-        tokenizer.add_bos_token=False
+        tokenizer.add_bos_token = False
     return model, tokenizer, model_config, wrapper
-
-
 
 
 def load_plm_from_config(config: CfgNode):
@@ -135,12 +138,12 @@ def load_plm_from_config(config: CfgNode):
         :obj:`model_config`: The wrapper class of this plm.
     """
     plm_config = config.plm
-    model_class = get_model_class(plm_type = plm_config.model_name)
+    model_class = get_model_class(plm_type=plm_config.model_name)
     model_config = model_class.config.from_pretrained(plm_config.model_path)
     # you can change huggingface model_config here
     # if 't5'  in plm_config.model_name: # remove dropout according to PPT~\ref{}
     #     model_config.dropout_rate = 0.0
-    if 'gpt' in plm_config.model_name: # add pad token for gpt
+    if 'gpt' in plm_config.model_name:  # add pad token for gpt
         if "<pad>" not in config.plm.specials_to_add:
             config.plm.specials_to_add.append("<pad>")
     model = model_class.model.from_pretrained(plm_config.model_path, config=model_config)
@@ -148,6 +151,7 @@ def load_plm_from_config(config: CfgNode):
     wrapper = model_class.wrapper
     model, tokenizer = add_special_tokens(model, tokenizer, specials_to_add=config.plm.specials_to_add)
     return model, tokenizer, model_config, wrapper
+
 
 def add_special_tokens(model: PreTrainedModel,
                        tokenizer: PreTrainedTokenizer,
@@ -174,6 +178,3 @@ def add_special_tokens(model: PreTrainedModel,
                 model.resize_token_embeddings(len(tokenizer))
                 logger.info("pad token is None, set to id {}".format(tokenizer.pad_token_id))
     return model, tokenizer
-
-
-
